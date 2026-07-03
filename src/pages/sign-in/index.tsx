f1e928e1b7df3cc2b9c0023a352e5c5ffad333fa
Zeeshan Iqbal <zeeshan.iqbal.cs@gmail.com>
2026-07-03 16:22:08 +0500

    chore: clean the project

--- src/pages/sign-in/index.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/clerk-react';
-
-export default function SignInPage() {
-  return (
-    <div className="min-h-screen grid place-items-center p-4">
-      <SignIn routing="path" path="/sign-in" />
-    </div>
-  );
-}
--- src/pages/sign-up/index.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/clerk-react';
-
-export default function SignUpPage() {
-  return (
-    <div className="min-h-screen grid place-items-center p-4">
-      <SignUp routing="path" path="/sign-up" />
-    </div>
-  );
-}
